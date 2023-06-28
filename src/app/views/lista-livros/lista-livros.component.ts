163c172f043b672f4eee029343790af571542823
adcavalcant <aderc19@gmail.com>
2023-06-27 23:15:24 -0300

    Iniciando a Implementação da Busca

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
-
   listaLivros: [];
-
-  constructor() { }
-
+  campoBusca: string = '';
+  constructor(private livroService: LivroService) {}
+  buscarLivros() {
+    this.livroService.buscar(this.campoBusca);
+  }
 }
-
-
-
